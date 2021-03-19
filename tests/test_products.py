44bfc4e7d1139cfdf6c2c0864a5ca6a4482b8311
Brodie Hay <brodiehay2334@gmail.com>
2021-03-19 15:30:54 -0700

    tests for promotions.py and products.py created

---
+++ tests/test_products.py
@@ -0,0 +1,9 @@
+from GroceryCo_CheckoutSystem.products import products_dict, insert_products
+
+
+def test_answer():
+    str = "apple 1 orange 2 watermelon 5"
+    assert insert_products(str) == {'apple':{'key': "apple", 'price': 1},
+                                'orange':{'key': "orange", 'price': 2},
+                                'watermelon': {'key': "watermelon", 'price': 5}
+                                }
