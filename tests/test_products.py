b92a3b1a4b2f16099489b502d7050c7dd2282764
Brodie Hay <brodiehay2334@gmail.com>
2021-03-19 16:10:24 -0700

    fixing test cases after import changes to checkout.py

--- tests/test_products.py
+++ tests/test_products.py
@@ -2,8 +2,8 @@ from GroceryCo_CheckoutSystem.products import insert_products
 
 
 def test_answer():
-    str = "apple 1 orange 2 watermelon 5"
-    assert insert_products(str) == {'apple':{'key': "apple", 'price': 1},
+    user_input = "apple 1 orange 2 watermelon 5"
+    assert insert_products() == {'apple':{'key': "apple", 'price': 1},
                                 'orange':{'key': "orange", 'price': 2},
                                 'watermelon': {'key': "watermelon", 'price': 5}
                                 }
--- tests/test_promotions.py
+++ tests/test_promotions.py
@@ -1,8 +1,8 @@
 from GroceryCo_CheckoutSystem.promotions import insert_promotions
 
 def test_answer():
-    str = "apple 2 BOGO50 1.5 orange 6 6for10 10 watermelon 2 BOGOFree 5"
-    assert insert_promotions(str) == {'apple':{'key': 'apple', 'amount': 2, 'promo': 'BOGO50', 'price': 1.5},
+    user_input = "apple 2 BOGO50 1.5 orange 6 6for10 10 watermelon 2 BOGOFree 5"
+    assert insert_promotions() == {'apple':{'key': 'apple', 'amount': 2, 'promo': 'BOGO50', 'price': 1.5},
                                 'orange':{'key': 'orange', 'amount': 6, 'promo': '6for10', 'price': 10},
                                 'watermelon': {'key': 'watermelon', 'amount': 2, 'promo': 'BOGOFree', 'price': 5}
                                 }
