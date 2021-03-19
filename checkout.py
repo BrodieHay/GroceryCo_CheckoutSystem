d10ad5496378524f1be2105d2d9291375808b5b0
Brodie Hay <brodiehay2334@gmail.com>
2021-03-19 15:33:49 -0700

    global var bag removed, local vars for bag, promo, and prod created

--- checkout.py
+++ checkout.py
@@ -1,123 +1,116 @@
-#!/usr/bin/env python
-
 import math
 import sys
-from products import Products
-from products import insertProducts
-from promotions import Promotions
-from promotions import insertPromotions
+from products import insert_products
+from promotions import insert_promotions
 
-#Get the grocery Bag ready for the scanned products
-Bag = {}
-
-def getEmployeeId():
-    #Program expects ane employee to log in and enter new Products and Promotions (Promotions are optional)
-    idValid = False
-    promoValid = False
-    while not(idValid):
-        userInput = input("If you are an Employee and would like to update pricing and promotions, enter Employee ID (e.g. 1234):\n")
+def get_employee_id(products_dict, promotions_dict):
+    #Program expects ane employee to log in and enter new products and promotions (promotions are optional)
+    id_valid = False
+    promo_valid = False
+    while not(id_valid):
+        user_input = input("If you are an Employee and would like to update pricing and promotions, enter Employee ID (e.g. 1234):\n")
         #get the employees id and checks that it is valid
-        if userInput == "1234":
-            idValid = True
+        if user_input == "1234":
+            id_valid = True
             #set id flag to true and prompt employees to enter products
-            getItems()
-            while not(promoValid):
-                #check if there are any promotions that need to be entered
-                userInput=input("Are there any promotions? (Y/N)")
-                if userInput == "Y" or userInput == "y":
-                    promoValid = True
-                    getPromos()
-                elif userInput == "N" or userInput == "n":
-                    promoValid = True
-                else:
-                    print("That was not a valid input, please try again")
+            products_dict = get_items()
+            #check if there are any promotions that need to be entered
+            user_input=input("Are there any promotions? (Y/N)\n")
+            if user_input == "Y" or user_input == "y":
+                promo_valid = True
+                promotions_dict = get_promos()
+            elif user_input == "N" or user_input == "n":
+                promo_valid = True
+            else:
+                print("That was not a valid input, please try again")
         else:
             #employee id invalid, prompt again
             print("That was not a valid ID, please try again\n")
-    return
+    return products_dict, promotions_dict
 
-def getItems():
+def get_items():
     #gets user input from the shop employee to populate the product dictionary in products.py
-    userInput = input("Please enter the pricing rules in format 'Product Price' (e.g. apple 0.75 orange 1):\n")
-    insertProducts(userInput)
-    return
+    return insert_products()
 
-def getPromos():
-    #gets user input from the shop employee to populate the promotions dictionary in promotions.py
-    userInput = input("Please enter any promotions in format 'Product Amount Promotion Price' (e.g. apple 2 BOGO50 1):\n")
-    insertPromotions(userInput)
-    return
 
-def getGroceryList():
+def get_promos():
+    #gets user input from the shop employee to populate the promotions_dict dictionary in promotions.py
+    return insert_promotions()
+
+def get_grocery_list(bag, products_dict):
     #Gets the customers grocery list from the command line input
-    userInput = input("Please enter your grocery list:\n")
+    user_input = input("Please enter your grocery list:\n")
     #Create a shopping cart to hold the groceries
-    cart = list(map(str, userInput.split()))
+    cart = list(map(str, user_input.split()))
     #Put all of the groceries into the cart
     i = 0
     for item in cart:
-        loweredItem = item.lower()
-        cart[i] = loweredItem
+        lowered_item = item.lower()
+        cart[i] = lowered_item
         i+=1
     #Start checking out the items in the cart
 
     for item in cart:
-        if item in Products:
+        if item in products_dict:
             #item exists in inventory
-            if item in Bag:
+            if item in bag:
                 #check if there are already same items in the cart
-                Bag[item]['amount']+=1
-                Bag[item]['price']+=Products[item]['price']
+                bag[item]['amount']+=1
+                bag[item]['price']+=products_dict[item]['price']
             else:
-                #if this is the first time the Bag is seeing this item we need to initialize it
+                #if this is the first time the bag is seeing this item we need to initialize it
                 amount = 1
-                price = Products[item]['price']
-                Bag[item] = {'amount': amount, 'price': price}
+                price = products_dict[item]['price']
+                bag[item] = {'amount': amount, 'price': price}
         else:
-            #Throw error for none existent items but continue to checkout
+            #Tell customer about none existent items but continue to checkout
             print(item,"does not exist in inventory")
-    return
+    return bag
 
-def applyPromos():
-    #check if a promotion exists for each item in Bag and apply existing promos
-    for item in Bag:
-        promoList = ""
-        if item in Promotions:
+def apply_promos(bag, products_dict, promotions_dict, promo_list):
+    #check if a promotion exists for each item in bag and apply existing promos
+    for item in bag:
+        if item in promotions_dict:
             #promo exists for this item
-            if Bag[item]['amount'] >= Promotions[item]['amount']:
+            if bag[item]['amount'] >= promotions_dict[item]['amount']:
                 #check if customer is buying enough items for promo to apply
-                x = Bag[item]['amount'] / Promotions[item]['amount']
-                if x.is_integer():
+                promo_amount = bag[item]['amount'] / promotions_dict[item]['amount']
+                if promo_amount.is_integer():
                     #if all items apply for the promo
-                    Bag[item]['price'] = Promotions[item]['price'] * x
+                    bag[item]['price'] = promotions_dict[item]['price'] * promo_amount
                 else:
                     #if only an amount of the items apply for the discount
-                    #   e.g. 2 for 1 but 3 items in Bag
-                    xRounded = math.floor(x)
-                    Bag[item]['price'] = Bag[item]['price'] - (x * Products[item]['price'])
-                    Bag[item]['price'] = Bag[item]['price'] + (x * Promotions[item]['price'])
-                promoList = 'Promotion: {:s} applied to {:s}\n'.format(Promotions[item]['promo'],Products[item]['key'])
-    return promoList
+                    #   e.g. 2 for 1 but 3 items in bag
+                    promo_amount_rounded = math.floor(promo_amount)
+                    bag[item]['price'] = bag[item]['price'] - (promotions_dict[item]['amount'] * products_dict[item]['price'])
+                    bag[item]['price'] = bag[item]['price'] + (promo_amount_rounded * promotions_dict[item]['price'])
+                promo_list = promo_list + 'Promotion: {:s} applied to {:s}\n'.format(promotions_dict[item]['promo'],products_dict[item]['key'])
+    return promo_list
 
-def checkout(promoList):
+def checkout(bag, products_dict, promotions_dict, promo_list):
     #Takes the Grocery list provided by customer and and displays all the items in a sorted list
     #   along with a list of the promotions applied and the total cost
     print("Checking out...\n")
     sum=0
     print('{:<10s}{:>4s}{:>10s}{:>12s}'.format("Amount","Item","Each","Subtotal"))
-    for item in Bag:
+    for item in bag:
         #formatting for the list (can be adjusted easily depending width of recipt)
-        print('  {:<8d}{:<10s}{:^5.2f}{:^15.2f}\n'.format(Bag[item]['amount'],Products[item]['key'],Products[item]['price'],Bag[item]['price']))
-        sum+=Bag[item]['price']
-    print(promoList)
+        print('  {:<8d}{:<10s}{:^5.2f}{:^15.2f}'.format(bag[item]['amount'],products_dict[item]['key'],products_dict[item]['price'],bag[item]['price']))
+        sum+=bag[item]['price']
+    print("\n",promo_list)
     print("The total cost is: ${:.2f}".format(sum))
     return
 
 def main():
+    bag = {}
+    products_dict = {}
+    promotions_dict = {}
+    promo_list = ""
     print("\nWelcome to the GroceryCo Checkout system!\n")
-    getEmployeeId()
-    getGroceryList()
-    promoList = applyPromos()
-    checkout(promoList)
+    products_dict, promotions_dict = get_employee_id(products_dict, promotions_dict)
+    bag = get_grocery_list(bag, products_dict)
+    promo_list = apply_promos(bag, products_dict, promotions_dict, promo_list)
+    checkout(bag, products_dict, promotions_dict, promo_list)
 
-main()
+if __name__ == "__main__":
+    main()
